b87f81cff16fbe69d57fa5bada5e0ad6c2385e55
Martin Fox <martinf@kainos.com>
2023-10-16 13:13:36 +0100

    testing workflow as a multi-stage instead of matrix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -19,7 +19,27 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   skip_provider_registration = "true"
-  features {}
+  features {
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = true
+    }
+
+    virtual_machine_scale_set {
+      force_delete                  = true
+      roll_instances_when_required  = true
+      scale_to_zero_before_deletion = false
+    }
+  }
 }
 
 provider "cloudinit" {
